ead5982e49111216231a3f968d91f8f130796565
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-28 22:54:59 +0300

    Context

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,16 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
+
+export const ContextProvider = ({ children }) => {
+  return (
+    <StateContext.Provider value={{ test: "test" }}></StateContext.Provider>
+  );
+};
