f530c558676eca480316bd8b4068f76af638d239
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-28 23:54:25 +0300

    Nav items

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -40,10 +40,18 @@ const Navbar = () => {
       />
       <div className="flex">
         <NavButton
-          title="cart"
+          title="Cart"
           // customFunc={() => handleClick("cart")}
           color="blue"
           icon={<FiShoppingCart />}
+        />
+
+        <NavButton
+          title="Chat"
+          dotColor="#03C9D7"
+          // customFunc={() => handleClick("chat")}
+          color="blue"
+          icon={<BsChatLeft />}
         />
       </div>
     </div>
