fe365f6af31dd54313344d7732bbd3ebd248ea19
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-28 20:54:40 +0300

    SUB

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./componets";
 
+//imports from index
 import {
   Orders,
   Ecommerce,
