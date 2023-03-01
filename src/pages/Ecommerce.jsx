3d700060f0d82b161cad449c5fe0ba3f38bd778f
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-01 12:56:15 +0300

    SparkLine

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -70,11 +70,50 @@ const Ecommerce = () => {
           <div className="flex justify-between">
             <p className="font-semibold text-xl"> Revenue Updates</p>
             <div className=" flex items-center gap-4">
-              <p>
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
                 <span>
                   <GoPrimitiveDot />
                 </span>
+                <span>Expense</span>
               </p>
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="text-3xl font-semibold">$93,438</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3">
+                    23%
+                  </span>
+                </p>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+              <div className="mt-8">
+                <p>
+                  <span className="text-3xl font-semibold">$48,438</span>
+                </p>
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+
+              <div className="mt-5">
+                <SparkLine
+                  currentColor="blue"
+                  id="line-sparkline"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                />
+              </div>
             </div>
           </div>
         </div>
