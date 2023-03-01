eed08849986c6ff011a80512aab072ca0fcec5cf
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-01 12:30:58 +0300

    G-Card

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -65,6 +65,20 @@ const Ecommerce = () => {
           ))}
         </div>
       </div>
+      <div className=" flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl"> Revenue Updates</p>
+            <div className=" flex items-center gap-4">
+              <p>
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
