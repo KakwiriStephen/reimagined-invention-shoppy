8d19078f727ff2352b39847421ee4611dc146eb0
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-01 12:15:28 +0300

    Cards

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -52,6 +52,15 @@ const Ecommerce = () => {
               >
                 {item.icon}
               </button>
+
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+
+              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
             </div>
           ))}
         </div>
