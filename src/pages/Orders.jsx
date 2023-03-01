4c0c1e9bc93f9e4d07bdc22c01774c63e3aa5f2c
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-01 20:11:28 +0300

    Sort

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -21,12 +21,29 @@ const Orders = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="page" title="Orders" />
-      <GridComponent>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
       </GridComponent>
     </div>
   );
