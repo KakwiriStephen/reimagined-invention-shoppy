fa165c04781d2f850da25e9f83acd6ccb0aa6295
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-01 15:33:05 +0300

    Stacked

--- src/componets/Charts/Stacked.jsx
+++ src/componets/Charts/Stacked.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const Stacked = () => {
-  return <div>Stacked</div>;
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummy";
+
+const Stacked = ({ width, height }) => {
+  return (
+    <ChartComponent width={width} height={height}>
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+    </ChartComponent>
+  );
 };
 
 export default Stacked;
