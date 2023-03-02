e7c8735ca08bc6a70083a5e1ba8357382f9c1f54
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-02 15:34:55 +0300

    Calender App

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,4 +1,22 @@
 import React from "react";
+import {
+  ScheduleComponent,
+  ViewDirective,
+  ViewDirectives,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-schedule";
+
+import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
+
+import { scheduleData } from "../data/dummy";
+import { Header } from "../componets";
 
 const Calender = () => {
   return <div>Calender</div>;
