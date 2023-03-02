c3f57537a11aac8181474ed6dc07ba6e45f1ec8c
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-02 23:26:11 +0300

    colors

--- src/componets/ThemeSettings.jsx
+++ src/componets/ThemeSettings.jsx
@@ -53,6 +53,34 @@ const ThemeSettings = () => {
             </label>
           </div>
         </div>
+
+        <div className="flex-col border-t-1 border-color p-4 ml-4">
+          <p className=" font-semibold text-lg">Theme Colors</p>
+          <div className=" flex gap-3">
+            {themeColors.map((item, index) => (
+              <TooltipComponent
+                key={index}
+                content={item.name}
+                position="TopCenter"
+              >
+                <div className="relative mt-2 cursor-pointer flex gap-5 items-center">
+                  <button
+                    type="button"
+                    className="h-10 w-10 rounded-full cursor-pointer"
+                    style={{ backgroundColor: item.color }}
+                    onClick={() => {}}
+                  >
+                    <BsCheck
+                      className={`ml-2 text-2xl text_white ${
+                        false ? "block" : "hidden"
+                      }`}
+                    />
+                  </button>
+                </div>
+              </TooltipComponent>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
