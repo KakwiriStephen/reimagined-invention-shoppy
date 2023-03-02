290b31fdfc3009b03949aa00b59004b8d820b1a6
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-02 22:44:55 +0300

    Theme Settings

--- src/componets/ThemeSettings.jsx
+++ src/componets/ThemeSettings.jsx
@@ -9,7 +9,19 @@ import { useStateContext } from "../contexts/ContextProvider";
 const ThemeSettings = () => {
   return (
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
-      <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400"></div>
+      <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400">
+        <div className=" flex justify-between items-center p-4 ml-4">
+          <p className="font-semibold text-lg">Settings</p>
+          <button
+            type="button"
+            onClick={() => {}}
+            style={{ color: "rgb(153,171,180)", borderRadius: "50%" }}
+            className="text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray"
+          >
+            <MdOutlineCancel />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
