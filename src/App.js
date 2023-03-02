ba1b4d8a8897d975e5922fed991ec635835368d2
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-03 01:07:46 +0300

    DArk

--- src/App.js
+++ src/App.js
@@ -29,11 +29,16 @@ import {
 import "./App.css";
 
 const App = () => {
-  const { activeMenu, themeSettings, setThemeSettings, currentColor } =
-    useStateContext();
+  const {
+    activeMenu,
+    themeSettings,
+    setThemeSettings,
+    currentColor,
+    currentMode,
+  } = useStateContext();
 
   return (
-    <div>
+    <div className={currentMode === "Dark" ? "dark" : ""}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4 " style={{ zIndex: "1000" }}>
