c2aa5e1ccfddfc6e06fbed89bb29b821a4c044f5
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-02 23:54:48 +0300

    Theme Colors

--- src/componets/ThemeSettings.jsx
+++ src/componets/ThemeSettings.jsx
@@ -7,6 +7,9 @@ import { themeColors } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const ThemeSettings = () => {
+  const { setColor, setMode, currentMode, currentColor, setThemeSettings } =
+    useStateContext();
+
   return (
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
       <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400">
@@ -14,7 +17,7 @@ const ThemeSettings = () => {
           <p className="font-semibold text-xl">Settings</p>
           <button
             type="button"
-            onClick={() => {}}
+            onClick={() => setThemeSettings(false)}
             style={{ color: "rgb(153,171,180)", borderRadius: "50%" }}
             className="text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray"
           >
