0f38454d1c6c160af5101885d205e1a28bef1fd3
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-02 22:29:53 +0300

    Theme Settings

--- src/componets/ThemeSettings.jsx
+++ src/componets/ThemeSettings.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { MdOutlineCancel } from "react-icons/md";
 import { BsCheck } from "react-icons/bs";
-import { TooltipComponent } from "@asyncfusion/ej2-react-popups";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import { themeColors } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const ThemeSettings = () => {
-  return <div>ThemeSettings</div>;
+  return (
+    <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
+      <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400"></div>
+    </div>
+  );
 };
 
 export default ThemeSettings;
