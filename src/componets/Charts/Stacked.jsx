3e55028c61053cf632f504f179c8155dd96323e9
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-04 23:15:46 +0300

    Bug

--- src/componets/Charts/Stacked.jsx
+++ src/componets/Charts/Stacked.jsx
@@ -15,21 +15,26 @@ import {
   stackedPrimaryXAxis,
   stackedPrimaryYAxis,
 } from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const Stacked = ({ width, height }) => {
+  const { currentMode } = useStateContext();
+
   return (
     <ChartComponent
-      width={width}
-      height={height}
       id="charts"
       primaryXAxis={stackedPrimaryXAxis}
       primaryYAxis={stackedPrimaryYAxis}
+      width={width}
+      height={height}
       chartArea={{ border: { width: 0 } }}
       tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#33373E" : "#fff"}
       legendSettings={{ background: "white" }}
     >
-      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
       <SeriesCollectionDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         {stackedCustomSeries.map((item, index) => (
           <SeriesDirective key={index} {...item} />
         ))}
--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import {
   ScheduleComponent,
-  ViewDirective,
-  ViewDirectives,
   Day,
   Week,
   WorkWeek,
