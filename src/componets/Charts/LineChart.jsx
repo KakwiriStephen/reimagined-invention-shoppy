84cbdff4cde18dd7c17b723728395247d2999af5
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-04 14:28:49 +0300

    Chart

--- src/componets/Charts/LineChart.jsx
+++ src/componets/Charts/LineChart.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  LineSeries,
+  DateTime,
+  Legend,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  lineCustomSeries,
+  LinePrimaryXAxis,
+  LinePrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const LineChart = () => {
-  return <div>LineChart</div>;
+  return <ChartComponent></ChartComponent>;
 };
 
 export default LineChart;
